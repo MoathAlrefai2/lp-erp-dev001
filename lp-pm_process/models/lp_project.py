239a930ae7317f5cee9140fc9e7859949adcf2eb
Moath Arefai <MoathA@leading-point.com>
2021-02-21 17:30:31 +0200

    Add "Team Member", "TEAMS Link", "DevOps Link", and "Status" properties into Project model

--- lp-pm_process/models/lp_project.py
+++ lp-pm_process/models/lp_project.py
@@ -12,3 +12,13 @@ class LP_Project(models.Model):
          ('outsourcing', 'Outsourcing'),
          ('Inhouse', 'Inhouse')],
         'Type', default="fixed_time_price")
+
+    member_ids = fields.Many2many('res.partner', string='Team Members')
+    teams_link = fields.Char('TEAMS Channel')
+    devops_link = fields.Char('DevOps Project')
+    status = fields.Selection(
+        [('new', 'New'),
+         ('in_progress', 'In Progress'),
+         ('closed', 'Closed')],
+        'Status', default="new")
+
