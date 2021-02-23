bf315f5017f68f1df3bd15c0ceb122c3f1c33dcb
Moath Arefai <MoathA@leading-point.com>
2021-02-23 18:19:15 +0200

    Add Budget and Timeline properties into Project model

--- lp-pm_process/models/lp_project.py
+++ lp-pm_process/models/lp_project.py
@@ -6,19 +6,22 @@ from odoo import models, fields, api
 class LP_Project(models.Model):
     _inherit = 'project.project'
 
-    type = fields.Selection(
-        [('fixed_time_price', 'Fixed Time/Price'),
-         ('change_request', 'Change Request'),
-         ('outsourcing', 'Outsourcing'),
-         ('Inhouse', 'Inhouse')],
-        'Type', default="fixed_time_price")
+    lp_type = fields.Selection(
+        [('lp_fixed_time_price', 'Fixed Time/Price'),
+         ('lp_change_request', 'Change Request'),
+         ('lp_outsourcing', 'Outsourcing'),
+         ('lp_Inhouse', 'Inhouse')],
+        'Type', default="lp_fixed_time_price")
 
-    member_ids = fields.Many2many('res.partner', string='Team Members')
-    teams_link = fields.Char('TEAMS Channel')
-    devops_link = fields.Char('DevOps Project')
-    status = fields.Selection(
-        [('new', 'New'),
-         ('in_progress', 'In Progress'),
-         ('closed', 'Closed')],
-        'Status', default="new")
+    lp_member_ids = fields.Many2many('res.partner', string='Team Members')
+    lp_teams_link = fields.Char('TEAMS Channel')
+    lp_devops_link = fields.Char('DevOps Project')
+    lp_status = fields.Selection(
+        [('lp_new', 'New'),
+         ('lp_in_progress', 'In Progress'),
+         ('lp_closed', 'Closed')],
+        'Status', default="lp_new")
+
+    lp_budget = fields.Char('Budget')
+    lp_date_end = fields.Date('End Date')
 
