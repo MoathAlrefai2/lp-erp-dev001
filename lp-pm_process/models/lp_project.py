b2b09d6a3c04bbd579b856cdc709cae7dcc55000
Moath Arefai <MoathA@leading-point.com>
2021-03-03 18:12:46 +0200

    Add Proposed Budget & Timeline values Show log & messages in project view

--- lp-pm_process/models/lp_project.py
+++ lp-pm_process/models/lp_project.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 
 
 class LP_Project(models.Model):
-    _inherit = 'project.project'
+    _inherit = ['project.project']
 
     lp_type = fields.Selection(
         [('lp_fixed_time_price', 'Fixed Time/Price'),
@@ -22,7 +22,20 @@ class LP_Project(models.Model):
          ('lp_closed', 'Closed')],
         'Status', default="lp_new")
 
-    lp_budget = fields.Char('Budget')
-    lp_date_end = fields.Date('End Date')
+    lp_budget = fields.Char('Budget', readonly=True)
+    lp_date_end = fields.Date('End Date', readonly=True)
     lp_department_head = fields.Many2one('res.users', string='Department Head')
+
+    lp_proposed_budget = fields.Char('Proposed Budget')
+    lp_proposed_date_start = fields.Date('Proposed Start Date')
+    lp_proposed_date_end = fields.Date('Proposed End Date',)
+
+
+    #def pick_one(self):
+    #    """ This method used to customize display name of the record """
+    #    result = []
+    #    for record in self:
+    #        rec_name = "%s (%s)" % (record.name, record.date_start)
+    #        result.append((record.id, rec_name))
+    #    return result
 
