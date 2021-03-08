ae58debc6a27ebc57ad6fa0104269432e74d636c
Moath Arefai <MoathA@leading-point.com>
2021-03-08 18:14:14 +0200

    Add department head security group to Project model and apply it to Approve button

--- lp-pm_process/__manifest__.py
+++ lp-pm_process/__manifest__.py
@@ -15,12 +15,14 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'PM',
+    'category': 'Services/Project',
     'version': '14.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['project'],
     'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
         'views/lp_project.xml',
     ],
 }
