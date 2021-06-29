71847b9beb2ac550848148721b1b75a4061b7531
osama athamneh <osamaalyassin@yahoo.com>
2021-06-29 23:17:15 +0300

    test code review

--- lp-pm_process/models/lp_project.py
+++ lp-pm_process/models/lp_project.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#import logging
+import logging
 from urllib.parse import urlparse
 from odoo import models, fields, api
 from odoo.exceptions import AccessError, ValidationError
@@ -11,7 +11,7 @@ from vsts.vss_connection import VssConnection
 from msrest.authentication import BasicAuthentication
 from vsts.work_item_tracking.v4_1.models.wiql import Wiql
 
-#_logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 class LP_Project(models.Model):
     _inherit = ['project.project']
@@ -113,22 +113,8 @@ class LP_Project(models.Model):
             #Log the change into history
         else:
             raise AccessError('Only Admin or project\'s DH can approve the change')
-        
-    #def emit(msg, *args):
-    #    #print(msg % args)
-    #    _logger.warning(msg % args)
-
-
-    #def print_work_item(work_item):
-    #    emit(
-    #    "{0} {1}: {2}".format(
-    #        work_item.fields["System.WorkItemType"],
-    #        work_item.id,
-    #        work_item.fields["System.Title"],
-    #    ))
 
     def sync_devops_task(self, devops_task, odoo_task):
-        # do nothing
         odoo_task.lp_devops_ref_id = devops_task.id
 
 
@@ -147,13 +133,11 @@ class LP_Project(models.Model):
 
         wit_client = connection.get_client('vsts.work_item_tracking.v4_1.work_item_tracking_client.WorkItemTrackingClient')
         wiql_results = wit_client.query_by_wiql(wiql).work_items
-
         if wiql_results:
             # WIQL query gives a WorkItemReference with ID only
             # => we get the corresponding WorkItem from id
 
             tasks_to_be_synced = []
-            #odoo_tasks = self.env['project.task']
             odoo_task_ids = self.env['project.task'].search([('project_id', '=', self.id)]).ids
             for task in self.env['project.task'].browse(odoo_task_ids):
                 if task.lp_devops_ref_id: #odoo task already connected to devops task
@@ -161,32 +145,177 @@ class LP_Project(models.Model):
                     self.sync_devops_task(work_item, task)
                     tasks_to_be_synced.append(task.lp_devops_ref_id)
 
-            tasks_to_be_inserted = [wit_client.get_work_item(int(tmp_devops_task.id)) for tmp_devops_task in wiql_results if tmp_devops_task.id not in tasks_to_be_synced]
+            tasks_to_be_inserted= [wit_client.get_work_item(int(tmp_devops_task.id)) for tmp_devops_task in wiql_results ]
+            count_insert = 0
+            count_updates = 0
+            count_error_task = 0
             for work_item in tasks_to_be_inserted:
-                #print_work_item(work_item)
-                #_logger.warning("{0} {1}: {2}".format(
-                #    work_item.fields["System.WorkItemType"],
-                #    work_item.id,
-                #    work_item.fields["System.Title"]))
                 tmp_description = ''
+                tmp_TeamProject = ''
+                tmp_Changedate = ''
+                tmp_area = ''
+                tmp_IterationPath = ''
+                tmp_CreatedDate = ''
+                tmp_OriginalEstimate = ''
+                tmp_RemainingWork = ''
+                tmp_CompletedWork = ''
+                tmp_assigned_to = ''
+                tmp_finish_date = ''
+                tmp_priority = ''
+                tmp_Requirement =''
+                tmp_state = ''
+                tmp_state_changedate = ''
+                tags = ''
+                if "Microsoft.VSTS.Common.StateChangeDate" in work_item.fields:
+                    tmp_state_changedate = work_item.fields["Microsoft.VSTS.Common.StateChangeDate"][:19]
+                    tmp_state_changedate=tmp_state_changedate.replace('T',' ')
+                else:
+                    tmp_state_changedate = False
+                if "Microsoft.VSTS.CMMI.RequirementType" in work_item.fields:
+                    tmp_Requirement = work_item.fields["Microsoft.VSTS.CMMI.RequirementType"]
                 if "System.Description" in work_item.fields:
                     tmp_description = work_item.fields["System.Description"]
-
+                if "System.TeamProject" in work_item.fields:
+                    tmp_TeamProject = work_item.fields["System.TeamProject"]
+                if "System.ChangedDate" in work_item.fields:
+                    tmp_Changedate = work_item.fields["System.ChangedDate"][:19]
+                    tmp_Changedate = tmp_Changedate.replace('T', ' ')
+                if "System.AreaPath" in work_item.fields:
+                    tmp_area = work_item.fields["System.AreaPath"]
+                if "System.IterationPath" in work_item.fields:
+                    tmp_IterationPath = work_item.fields["System.IterationPath"]
+                if "System.CreatedDate" in work_item.fields:
+                    tmp_CreatedDate = work_item.fields["System.CreatedDate"]
+                if "Microsoft.VSTS.Common.ClosedDate" in work_item.fields:
+                    tmp_finish_date = work_item.fields["Microsoft.VSTS.Common.ClosedDate"]
+                else:
+                    tmp_finish_date = False
+                if "Microsoft.VSTS.Scheduling.OriginalEstimate" in work_item.fields:
+                    tmp_OriginalEstimate = work_item.fields["Microsoft.VSTS.Scheduling.OriginalEstimate"]
+                if "Microsoft.VSTS.Scheduling.RemainingWork" in work_item.fields:
+                    tmp_RemainingWork = work_item.fields["Microsoft.VSTS.Scheduling.RemainingWork"]
+                if "Microsoft.VSTS.Scheduling.CompletedWork" in work_item.fields:
+                    tmp_CompletedWork = work_item.fields["Microsoft.VSTS.Scheduling.CompletedWork"]
+                if "System.AssignedTo" in work_item.fields:
+                    tmp_assigned_to = self.search_user(self.id, work_item.fields["System.AssignedTo"])
+                if "Microsoft.VSTS.Common.Priority" in work_item.fields:
+                   tmp_priority = work_item.fields["Microsoft.VSTS.Common.Priority"]
+                else:
+                    tmp_priority = False
+                if "System.Tags" in work_item.fields:
+                    tags = work_item.fields["System.Tags"]
+                    tmp_tags = []
+                    if tags:
+                        tags = tags.split()
+                        for tag in tags:
+                            if tag[-1] == ';':
+                                tag = tag[:-1]
+                            if not self.env['project.tags'].sudo().search([('name', '=', tag)]):
+                                self.env['project.tags'].sudo().create({'name': tag})
+                            lp_s = self.env['project.tags'].search([('name', '=', tag)])
+                            tmp_tags.append((4,lp_s[0].id))
+                if "System.State" in work_item.fields:
+                    state = work_item.fields["System.State"]
+                    if state == 'Done':
+                        tmp_state = 'done'
+                    elif state == 'Active':
+                        tmp_state = 'active'
+                    elif state == 'To Do':
+                        tmp_state = 'to_do'
+                    elif state == 'Closed':
+                        tmp_state = 'closed'
+                    elif state == 'Resolved':
+                        tmp_state = 'resolved'
+                    else:
+                        tmp_state = ''
                 tmp_task = {
                     'name': work_item.fields["System.Title"],
                     'description': tmp_description,
+                    'lp_devops_completed_work': tmp_CompletedWork,
                     'project_id': self.id,
+                    'lp_devops_project_name': tmp_TeamProject,
+                    'lp_devops_changed_date': tmp_Changedate,
+                    'lp_devops_area': tmp_area,
+                    'lp_devops_iteration': tmp_IterationPath,
+                    'lp_devops_start_date': tmp_CreatedDate,
+                    'lp_devops_original_estimate': tmp_OriginalEstimate,
+                    'lp_devops_remaining_work': tmp_RemainingWork,
+                    'lp_devops_finish_date': tmp_finish_date,
+                    'user_id': tmp_assigned_to,
+                    'lp_devops_priority': tmp_priority,
+                    'date_last_stage_update': tmp_state_changedate,
+                     'lp_task_state': tmp_state,
+                    'lp_devops_requirement': tmp_Requirement,
                     'lp_devops_ref_id': work_item.id
-                    }
-                self.env['project.task'].create(tmp_task)
+                }
+                try:
+                 if work_item.id not in tasks_to_be_synced:
+                    record = self.env['project.task'].create(tmp_task)
+                    count_insert = count_insert + 1
+                    try:
+                        record.tag_ids = [(5, 0, 0)]
+                        if tmp_tags:
+                            record.tag_ids = tmp_tags
+                    except:
+                        pass
+                       # _logger.warning('WARNING: tags not valid on the task!')
+                 else:
+
+                  record_ids = self.env['project.task'].search([('lp_devops_ref_id', '=', work_item.id), ('project_id', '=', self.id)])
+
+                  for record in record_ids:
+                     if "System.ChangedDate" in work_item.fields:
+                       changed_date = work_item.fields["System.ChangedDate"][:19]
+                       changed_date =changed_date.replace('T',' ')
+                       if str(record.lp_devops_changed_date) != changed_date: #or str(record.changed state) != changed_date
+                         count_updates = count_updates + 1
+                         record.write(tmp_task)
+
+                         try:
+                            record.tag_ids = [(5, 0, 0)]
+                            if tmp_tags:
+                              record.tag_ids = tmp_tags
+                         except:
+                            pass
+                except:
+                    _logger.warning('WARNING: task not sync succssfully!')
+                    count_error_task = count_error_task+1
+
+            return {
+            'name': 'Information',
+            'type': 'ir.actions.act_window',
+            'res_model': 'project.wizard',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'context':{'default_lp_updates_counter':count_updates , 'default_lp_insert_counter':count_insert,'default_lp_error_counter':count_error_task},
+            'target': 'new'
+                }
+    def get_email(self, AssignedTo=''):
+        email = ''
+        try:
+            AssignedTo = str(AssignedTo).split()
+            email = AssignedTo[-1]
+           # email = email[1: -1]
+        except :
+            _logger.warning('WARNING: email not valid on this task!')
+        return email.strip("<>")
 
 
-            #work_items = (wit_client.get_work_item(int(res.id)) for res in wiql_results)
-            #for work_item in work_items:
-                ##print_work_item(work_item)
-                #_logger.warning("{0} {1}: {2}".format(
-                    #work_item.fields["System.WorkItemType"],
-                    #work_item.id,
-                    #work_item.fields["System.Title"]))
+    def search_user(self, project_id, AssignedTo=''):
+        email = self.get_email(AssignedTo)
+        users = self.env['res.users'].sudo().search([('login', '=', email)])
+        if users:
+            return users[0].id
+        else:
+            project_id = self.env['project.project'].sudo().search([('id', '=', project_id)])
+            if project_id:
+                return project_id[0].user_id.id
+            else:
+                ''
+class LP_Popup_Wizard(models.TransientModel):
+    _name = 'project.wizard'
+    lp_updates_counter = fields.Integer('Updated tasks:', readonly=True)
+    lp_insert_counter = fields.Integer('Inserted tasks:', readonly=True)
+    lp_error_counter = fields.Integer('Error tasks:', readonly=True)
 
 
--- lp-pm_process/models/lp_project_task.py
+++ lp-pm_process/models/lp_project_task.py
@@ -5,10 +5,10 @@ from odoo import models, fields, api
 class LP_Project_Task(models.Model):
     _inherit = ['project.task']
 
-
+    lp_devops_priority = fields.Integer('Priority', readonly=True)
     #DevOps fields
     lp_devops_ref_id = fields.Integer('Reference Id', readonly=True)
-    lp_devops_changed_date = fields.Date('Changed Date', readonly=True)
+    lp_devops_changed_date = fields.Datetime('Changed Date', readonly=True)
     lp_devops_project_name = fields.Char('Project Name', readonly=True)
     lp_devops_area = fields.Char('Area', readonly=True)
     lp_devops_iteration = fields.Char('Iteration', readonly=True)
@@ -26,3 +26,6 @@ class LP_Project_Task(models.Model):
     lp_devops_epic = fields.Char('Epic', readonly=True)
     lp_devops_feature = fields.Char('Feature', readonly=True)
     lp_devops_requirement = fields.Char('Requirement', readonly=True)
+
+    lp_selection_tasks = [('to_do', 'To Do'), ('active', 'Active'), ('done', 'Done'), ('closed', 'Closed'),('resolved', 'Resolved')]
+    lp_task_state = fields.Selection(lp_selection_tasks, string='State', readonly=True)
