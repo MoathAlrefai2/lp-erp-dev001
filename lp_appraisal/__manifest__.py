53934f44ea8960d33a1ff5ae6beb338f531d10b8
osama athamneh <osamaalyassin@yahoo.com>
2021-08-03 15:17:00 +0300

    modifications of Appraisal module

---
+++ lp_appraisal/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name':"lp_appraisal",
+    "description":'(lp_appraisal) inherit hr appraisal'  ,
+    'summary': """Modifications for apprasial module , https://leading-point.com/""",
+
+    'description': """
+      Modifications for appraisal
+  """,
+
+    'author': "Leading Point",
+    'website': "https://leading-point.com",
+ 'data': [
+     'security/ir.model.access.csv',
+'views/lp_appraisal.xml'
+    ],
+     'category': 'Tools',
+    'depends':
+        ['hr','survey','base']
+
+}
