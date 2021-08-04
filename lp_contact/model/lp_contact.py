a419a3db9034c008d21d7996a4cc60ab5f826edf
osama athamneh <osamaalyassin@yahoo.com>
2021-08-04 14:12:22 +0300

    fix bug on lp_contact :)

--- lp_contact/model/lp_contact.py
+++ lp_contact/model/lp_contact.py
@@ -10,28 +10,13 @@ class LP_Contact(models.Model):
                               ('lp_technical', 'Technical'),('lp_information_provider', 'Information Provider')],
                             'Position', default="lp_technical")
   lp_label_name = fields.Char('label for name.', default='ind_',readonly=True)
-  lp_name = fields.Char('name label')
+  lp_name = fields.Char('name label',compute='onchange_name')
 
-  def get_prefix_person(self,values):
+
+  @api.depends('lp_name')
+  def onchange_name(self):
       prefix = "ind_"
-      try:
-              values['lp_name'] = prefix + values['name']
-      except Exception as e:
-          _logger.exception(e)
-      return values
-  def write(self, values):
-   if self.company_type:
-    if self.company_type == 'person':
-         values = self.get_prefix_person(values)
-    else:
-         values['lp_name'] = self.name
-   return super(LP_Contact, self).write(values)
-
-  @api.model
-  def create(self, values):
-      if values['company_type']:
-        if values['company_type'] == 'person':
-            values = self.get_prefix_person(values)
-        else:
-            values['lp_name'] = self.name
-      return super(LP_Contact, self).create(values)
+      if self.company_type == 'person':
+       self.lp_name = prefix + self.name
+      else:
+        self.lp_name = self.name
